9c809d22e73d1696859757c66058e483b1a99481
dfenjves <fenjves.daniel@gmail.com>
2016-03-22 12:03:57 -0400

    basic tests but not working for stdout

---
+++ parrot_test.py
@@ -0,0 +1,25 @@
+import sys
+import unittest
+from mock import patch
+from io import StringIO
+from parrot import *
+
+class TestParrot(unittest.TestCase):
+
+    def test_parrot_no_args(self):
+        self.assertEqual(parrot(), "Squawk!")
+        # with patch('sys.stdout', new=StringIO()) as fake_out:
+        #     parrot()
+        #     self.assertEqual(fake_out.getvalue(), "Squawk")
+        # Need help figuring out how to test stdout...
+
+    def test_parrot_with_args(self):
+        self.assertEqual(parrot("hello"), "Pretty Bird!")
+        self.assertEqual(parrot("hi Parrot"), "Pretty Bird!")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
