7444f1386f98af1e51cf44a9be0a7a686e6a7aac
dfenjves <fenjves.daniel@gmail.com>
2016-04-08 13:43:15 -0400

    tests work

--- parrot.py
+++ parrot.py
@@ -1,3 +1,2 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-
--- parrot_test.py
+++ parrot_test.py
@@ -1,25 +1,41 @@
 import sys
 import unittest
-from mock import patch
-from io import StringIO
+# Note, for python 3, just do 'from io import StringIO'
+from io import BytesIO as StringIO
 from parrot import *
+
 
 class TestParrot(unittest.TestCase):
 
     def test_parrot_no_args(self):
-        self.assertEqual(parrot(), "Squawk!")
-        # with patch('sys.stdout', new=StringIO()) as fake_out:
-        #     parrot()
-        #     self.assertEqual(fake_out.getvalue(), "Squawk")
-        # Need help figuring out how to test stdout...
+        """It prints and returning Squawk if no arguments are given"""
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            parrot()
+            output = out.getvalue().strip()
+            self.assertEqual(output, 'Squawk!')
+            self.assertEqual(parrot(),'Squawk!')
+        finally:
+            sys.stdout = saved_stdout
+
 
     def test_parrot_with_args(self):
-        self.assertEqual(parrot("hello"), "hello")
-        self.assertEqual(parrot("hi Parrot"), "hi Parrot")
+        """It prints and returns the phrase if phrase if given as an argument"""
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            return_value = parrot("hello")
+            output = out.getvalue().strip()
+            self.assertEqual(output, 'hello')
+            self.assertEqual(return_value,'hello')
+        finally:
+            sys.stdout = saved_stdout
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
 
